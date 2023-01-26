07741483a0e75efbb0210a121c0424dff3d4fbe5
Muhammed-saber79 <muhammed.saber385@gmail.com>
2023-01-26 23:22:30 +0200

    second commit

--- dataManipulation.sh
+++ dataManipulation.sh
@@ -3,30 +3,39 @@ echo "hello Data Manipulation...!"
 
 if [ -d Databases ]
 then
-	if [ -d Databases/$1 ]
-	then 
-		read -p "Enter Table Name: " tableName
-		if [ -f Databases/$1/$tableName ]
-		then
+	clear
+	echo "******************** Tables Operations *********************"
+	if [[ $# -eq 1 ]]
+		if [ -d Databases/$1 ]
+		then 
 			clear
-				echo "*********************** Data Manipulation [CRUD] *************************"
-				select option in "Insert Data" "Select Data" "Updata Data" "Delete Data" "Add Column"  "Back To Main Menu"
-				do
-					case $REPLY in
-					1) ./insert.sh $1 $tableName;;
-					2) ./selectData.sh $1 $tableName;;
-					3) ./update.sh $1 $tableName;;
-					4) ./delete.sh $1 $tableName;;
-					5) ./addingColumn.sh $1 $tableName;;
-					6) ./index.sh;;
-					*) echo "Please Enter Valid Option...!";;
-					esac
-				done
-		else 
-				echo "No Such Table Exists...!"
+			echo "******************** Tables Options ON Database [$1] *********************"
+
+			read -p "Enter Table Name: " tableName
+			if [ -f Databases/$1/$tableName ]
+			then
+				clear
+					echo "*********************** Data Manipulation [CRUD] *************************"
+					select option in "Insert Data" "Select Data" "Updata Data" "Delete Data" "Add Column"  "Back To Previous Menu"
+					do
+						case $REPLY in
+						1) ./insert.sh $1 $tableName;;
+						2) ./selectData.sh $1 $tableName;;
+						3) ./update.sh $1 $tableName;;
+						4) ./delete.sh $1 $tableName;;
+						5) ./addingColumn.sh $1 $tableName;;
+						6) ./index.sh;;
+						*) echo "Please Enter Valid Option...!";;
+						esac
+					done
+			else 
+					echo "No Such Table Exists...!"
+			fi
+		else
+			echo "No Such Database Exists...!"
 		fi
+	then
 	else
-		echo "No Such Database Exists...!"
 	fi
 fi
 
